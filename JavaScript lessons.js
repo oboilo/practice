f211142bc029e9ef18adfb16c1645b6e1599fb76
oboilo <oleh.boilo@gmail.com>
2022-08-07 14:54:35 +0200

    Adding practice files

---
+++ JavaScript lessons.js
@@ -0,0 +1,43 @@
+/*console.log ("Testing in progress")
+console.log ("Test failed")
+console.log ("Hello!")
+console.log ("JS is fun")*/
+
+
+/*var age = 20;
+console.log (age >= 18)
+
+var hour = 10;
+time = hour >= 0 && hour <= 12 ? "am" : "pm" ; 
+document.write(time)
+
+function main() {
+    var hour = parseInt(readLine(), 13);
+    var clock = (hour < 12) ? "am" : "pm";
+    document.write(clock)
+}
+
+function main() {
+    var hour = parseInt(readLine(), 10);
+    var time = hour >= 0 && hour <= 12 ? "am" : "pm" ; 
+    document.write(time)
+}
+
+
+var hello = 42;
+var username = "Oleh";
+document.write(hello + username);*/
+
+/*var month = "August";
+if (month == "August"){
+    console.log("vacation")
+}*/
+
+
+
+var age = 1;
+if (age >= 18) {
+    console.log("Allowed")
+} else {
+    console.log("Denied")
+}
