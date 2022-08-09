7475310a5fa36a339ba5855901b903e12e8ed50a
oboilo <oleh.boilo@gmail.com>
2022-08-09 10:23:56 +0200

    js practice

--- JavaScript lessons.js
+++ JavaScript lessons.js
@@ -35,11 +35,82 @@ if (month == "August"){
 
 
 
-var age = 18;
+/*var age = 18;
 if (age > 18) {
     document.write("<h2>Allowed</h2>")
 } else if (age == 18 ) {
     document.write ("<h2>Maybe</h2>")
 } else {
     document.write("<h2>Denied</h2>")
-}
+}*/
+
+//var age = 42;
+//var isAdult = (age < 18) ? "Too young": "Old enough";
+//document.write(isAdult);
+
+/*var score = 39;
+if (score >= 88) {
+    console.log("excellent")
+} else if (score >= 40 && score <= 87) {
+    console.log ("good")
+} else {
+    console.log ("fail")
+}*/
+
+/*var day = 2;
+switch (day) {
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("Wednesday");
+        break;
+    default:
+        document.write("Another day");
+}*/
+
+/*var themeNumber = 2;
+switch (themeNumber) {
+    case 1:
+        console.log ("Light")
+        break;
+    case 2:
+        console.log ("Dark")
+        break;
+    case 3:
+        console.log ("Nocturne")
+        break;
+    case 4:
+        console.log ("Terminal")
+        break;
+    default:
+        console.log("Indigo")
+        break;
+}*/
+
+/*var age = 24;
+switch (age) {
+    case 24:
+        document.write(age);
+        break;
+    case 2:
+        document.write(age + 2);
+        break;
+    default:
+        document.write(age + 5);
+}*/
+/*var i = 1;
+for (i; i<=5; i++) {
+    document.write(i + "<br />");
+}*/
+
+/*var x = 0;
+for (; x <= 20; x += 2) {
+   document.write(x + "<br />"); 
+}*/
+
+const expression = 10 * (7 + 8 - 11) / 4;
+document.write(expression)
